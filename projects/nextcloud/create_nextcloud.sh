92290777eb2fa18d85aec5a06a303e9899b1c884
Rémi Cocula <rcocula@gmail.com>
2019-05-26 19:28:41 +0200

    WIP

--- projects/nextcloud/create_nextcloud.sh
+++ projects/nextcloud/create_nextcloud.sh
@@ -3,3 +3,5 @@
 . ./env.sh
 
 ${OC_COMMAND} process -f nextcloud.yaml -p NEXTCLOUD_HOST=${NEXTCLOUD_HOST} -p NEXTCLOUD_IMAGE_TAG=${NEXTCLOUD_IMAGE_TAG} | ${OC_COMMAND} -n ${PROJECT} create -f -
+
+${OC_COMMAND} start-build nextcloud --from-dir=build-nextcloud
